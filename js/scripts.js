1cf98b7f6f91006f0cc29db098f656e2d805ee88
Devin Cooley <dcooley1350@gmail.com>
2019-06-04 16:08:53 -0700

    add jquery and bootstrap files.

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,6 @@ var multiply = function(number1, number2) {return(number1 * number2)};
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 
-var result = add(number1, number2);
+var result = multiply(number1, number2);
 
 alert(result);
